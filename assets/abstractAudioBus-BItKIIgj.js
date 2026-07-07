aed843a46662509bdadceee2072c7658ce6a8236
Martin Wettig <31127009+Species521@users.noreply.github.com>
2026-07-07 13:38:02 +0200

    UI overlays, rebuild 13

---
+++ assets/abstractAudioBus-BItKIIgj.js
@@ -0,0 +1,15 @@
+import { Z as AbstractAudioOutNode } from './index-BbhUp-VV.js';
+
+/**
+ * Abstract class representing an audio bus with volume control.
+ *
+ * An audio bus is a node in the audio graph that can have multiple inputs and outputs. It is typically used to group
+ * sounds together and apply effects to them.
+ */
+class AbstractAudioBus extends AbstractAudioOutNode {
+    constructor(name, engine) {
+        super(name, engine, 3 /* AudioNodeType.HAS_INPUTS_AND_OUTPUTS */);
+    }
+}
+
+export { AbstractAudioBus as A };
---
+++ assets/oitFinalSimpleBlend.fragment-__J8CENz.js
@@ -0,0 +1,14 @@
+import { h as ShaderStore } from './index-BbhUp-VV.js';
+
+// Do not edit.
+const name = "oitFinalSimpleBlendPixelShader";
+const shader = `precision highp float;uniform sampler2D uFrontColor;void main() {ivec2 fragCoord=ivec2(gl_FragCoord.xy);vec4 frontColor=texelFetch(uFrontColor,fragCoord,0);glFragColor=frontColor;}
+`;
+// Sideeffect
+if (!ShaderStore.ShadersStore[name]) {
+    ShaderStore.ShadersStore[name] = shader;
+}
+/** @internal */
+const oitFinalSimpleBlendPixelShader = { name, shader };
+
+export { oitFinalSimpleBlendPixelShader };
