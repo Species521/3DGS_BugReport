a8a1ab50ea2eecc4cc63aeaae904e4ad6ed6c85a
Martin Wettig <31127009+Species521@users.noreply.github.com>
2026-07-07 10:37:54 +0200

    rebuild 09

---
+++ assets/gaussianSplatting.fragment-skLELRW-.js
@@ -0,0 +1,34 @@
+import { h as ShaderStore, i as clipPlaneFragmentDeclarationWGSL, l as logDepthDeclarationWGSL, j as fogFragmentDeclarationWGSL, k as logDepthFragmentWGSL, m as fogFragmentWGSL, n as gaussianSplattingFragmentDeclarationWGSL, o as clipPlaneFragmentWGSL } from './index-C_lbZlng.js';
+
+// Do not edit.
+const name = "gaussianSplattingPixelShader";
+const shader = `#include<clipPlaneFragmentDeclaration>
+#include<logDepthDeclaration>
+#include<fogFragmentDeclaration>
+varying vColor: vec4f;varying vPosition: vec2f;
+#define CUSTOM_FRAGMENT_DEFINITIONS
+#include<gaussianSplattingFragmentDeclaration>
+@fragment
+fn main(input: FragmentInputs)->FragmentOutputs {
+#define CUSTOM_FRAGMENT_MAIN_BEGIN
+#include<clipPlaneFragment>
+var finalColor: vec4f=gaussianColor(input.vColor,input.vPosition);
+#define CUSTOM_FRAGMENT_BEFORE_FRAGCOLOR
+fragmentOutputs.color=finalColor;
+#define CUSTOM_FRAGMENT_MAIN_END
+}
+`;
+// Sideeffect
+if (!ShaderStore.ShadersStoreWGSL[name]) {
+    ShaderStore.ShadersStoreWGSL[name] = shader;
+}
+const includes = [clipPlaneFragmentDeclarationWGSL, logDepthDeclarationWGSL, fogFragmentDeclarationWGSL, logDepthFragmentWGSL, fogFragmentWGSL, gaussianSplattingFragmentDeclarationWGSL, clipPlaneFragmentWGSL];
+for (const inc of includes) {
+    if (!ShaderStore.IncludesShadersStoreWGSL[inc.name]) {
+        ShaderStore.IncludesShadersStoreWGSL[inc.name] = inc.shader;
+    }
+}
+/** @internal */
+const gaussianSplattingPixelShaderWGSL = { name, shader };
+
+export { gaussianSplattingPixelShaderWGSL };
---
+++ assets/volumetricLightingRenderVolume.vertex-ByrCKw-Z.js
@@ -0,0 +1,23 @@
+import { h as ShaderStore, s as sceneUboDeclarationWGSL, p as meshUboDeclarationWGSL } from './index-C_lbZlng.js';
+
+// Do not edit.
+const name = "volumetricLightingRenderVolumeVertexShader";
+const shader = `#include<sceneUboDeclaration>
+#include<meshUboDeclaration>
+attribute position : vec3f;varying vWorldPos: vec4f;@vertex
+fn main(input : VertexInputs)->FragmentInputs {let worldPos=mesh.world*vec4f(vertexInputs.position,1.0);vertexOutputs.vWorldPos=worldPos;vertexOutputs.position=scene.viewProjection*worldPos;}
+`;
+// Sideeffect
+if (!ShaderStore.ShadersStoreWGSL[name]) {
+    ShaderStore.ShadersStoreWGSL[name] = shader;
+}
+const includes = [sceneUboDeclarationWGSL, meshUboDeclarationWGSL];
+for (const inc of includes) {
+    if (!ShaderStore.IncludesShadersStoreWGSL[inc.name]) {
+        ShaderStore.IncludesShadersStoreWGSL[inc.name] = inc.shader;
+    }
+}
+/** @internal */
+const volumetricLightingRenderVolumeVertexShaderWGSL = { name, shader };
+
+export { volumetricLightingRenderVolumeVertexShaderWGSL };
